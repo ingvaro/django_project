d3ba6a91c633f9a687c0919c8cab72df02719c78
Ingvar Ómarsson <ingvaro@gmail.com>
2014-12-17 04:53:06 +0000

    Updated database info, added ignore file for git

--- django_project/settings.py
+++ django_project/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'django_project',
+        'USER': 'root',
+        'PASSWORD': 'HrutsPungar13',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
